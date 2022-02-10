c35620ef9d7df41b6e7fd8203b021c42393779e5
Myriam-Mnnr <98812962+Myriam-Mnnr@users.noreply.github.com>
2022-02-10 14:39:54 +0100

    calcul de l'âge avec l'année de naissance, le mois et le jour

--- 20220209_bonjourjavascript/js/script.js
+++ 20220209_bonjourjavascript/js/script.js
@@ -5,13 +5,17 @@ function askName() {
     document.body.innerHTML += "<h2>" + nameofuser + "</h2>"; 
 }
 
-//Demander l'année de naissance, calculer l'age et l'afficher dans un h3
+//Demander l'année de naissance, le mois et le jour, calculer l'age et l'afficher dans un h3
 function askBirthYear() {
     let birthyear = prompt ("Quel est votre année de naissance ?");
-    //let birthmonth = prompt ("Quel est votre mois de naissance ?");
-    //let date = new Date().getFullYear
-    let age = 2022 - birthyear; 
-    document.body.innerHTML += "<h3> Vous avez " + age + " ans. </h3>"; 
+    let birthmonth = prompt ("Quel est votre mois de naissance ?");
+    let birthday = prompt("Quel est votre jour de naissance ?"); 
+    let today = new Date(); 
+    let birth = new Date(birthyear, birthmonth - 1, birthday); 
+    let age = today - birth; 
+    let agemonth = Math.round(age/1000/60/60/24/365.25); 
+
+    document.body.innerHTML += "<h3> Vous avez " + agemonth + " ans. </h3>"; 
 }
 
 askName(); 
